a4cf471f0a196cf882e189eb282e0e8d90bfa157
Tong <zhangt766@gmail.com>
2025-05-06 23:41:58 +1000

    Add files via upload

---
+++ test_lastfm.sh
@@ -0,0 +1,22 @@
+python main.py \
+--mode test \
+--batch_size 8 \
+--accumulate_grad_batches 16 \
+--dataset lastfm_data \
+--data_dir data/ref/lastfm \
+--cans_num 20 \
+--prompt_path ./prompt/artist.txt \
+--rec_embed SASRec \
+--llm_tuning lora \
+--llm_path /workspace/llama/models_hf/Llama-2-7b-hf \
+--rec_model_path ./rec_model/lastfm.pt \
+--ckpt_path ./checkpoints/lastfm.ckpt \
+--output_dir ./output/lastfm/ \
+--log_dir lastfm_logs \
+--lr_warmup_start_lr 7e-6 \
+--lr 7e-4 \
+--lr_decay_min_lr 7e-6 \
+--max_epochs 5 \
+--user_emb_path data/ref/lastfm/test_user_embedding.npy \
+--item_emb_path data/ref/lastfm/item_embedding.npy \
+--tau_path ./checkpoints/trained_tau.pth
